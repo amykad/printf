e1b6a9ee5d3be2cfd1ddd46bab5026f7c324ef98
Perla-dev <yumadev7@gmail.com>
2023-11-10 09:58:32 -0800

    Add main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+
+int _printf(const char *format, ...);
+
+int main(void);
+
+#endif
