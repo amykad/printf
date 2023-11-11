bba19e06093c3af3d680b474882c362ca8d46e7c
amykad <amina.kaddal@gmail.com>
2023-11-11 09:49:43 -0800

    apdate main.h

--- main.h
+++ main.h
@@ -7,6 +7,14 @@
 #include <stdio.h>
 #include <limits.h>
 
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list args);
+int get_precision(const char *format, int *i, va_list args);
+int get_size(const char *format, int *i);
+int handle_print(
+const char *format, int *i, va_list args,
+char buffer[], int flags, int width, int precision, int size
+);
 int _printf(const char *format, ...);
 void flush_output(char customBuffer[], int *buff_index);
 int main(void);
