a50d24b35d480e3a42358f3a7564f6d86c28df2b
amykad <amina.kaddal@gmail.com>
2023-11-15 10:52:49 -0800

    apdate

--- main.h
+++ main.h
@@ -7,9 +7,17 @@
 #include <stdio.h>
 #include <limits.h>
 
+#define S_LONG  sizeof(long)
+#define S_SHORT sizeof(short)
+
 int _putchar(char c);
 int print_integer(int n);
 int _printf(const char *format, ...);
+int get_size(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+int flags, int width, int precision, int size);
+
 
 int main(void);
 
